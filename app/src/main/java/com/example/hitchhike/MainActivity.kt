98a008260924f3b40c79c0d343a3d907f5f90afa
Muhammad Moiz Hussain <mohammadmoiz6@gmail.com>
2021-11-18 15:42:33 -0500

    Set App Orientation

--- app/src/main/java/com/example/hitchhike/MainActivity.kt
+++ app/src/main/java/com/example/hitchhike/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
