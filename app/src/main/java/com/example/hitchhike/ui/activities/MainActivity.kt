c4466c154f8ab070346da3df2ec3b0684b968f2d
Muhammad Moiz Hussain <mohammadmoiz6@gmail.com>
2022-03-01 15:16:25 -0500

    Search Ride Requests from Valid Trips only

--- app/src/main/java/com/example/hitchhike/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/hitchhike/ui/activities/MainActivity.kt
@@ -18,6 +18,7 @@ import com.example.hitchhike.ui.adapters.MyAdapter
 import com.example.hitchhike.R
 import com.example.hitchhike.model.ScheduleRequestInfo
 import com.example.hitchhike.model.TripsInfo
+import com.example.hitchhike.ui.interfaces.MyCallBack
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 import com.google.firebase.auth.FirebaseAuth
@@ -28,14 +29,11 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
 
-//Don't delete rides, just show those rides which dates are not passed.
-// SEarch for soft-deletes
+// Don't delete rides, just show those rides which dates are not passed.
+// Search for soft-deletes
 
 class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavigationItemSelectedListener {
 
-//    private val radioButtonDriver: RadioButton by lazy { findViewById(R.id.radioBtnDriver) }
-//    private val radioButtonRider: RadioButton by lazy { findViewById(R.id.radioBtnRider) }
-//    private var userType: String? = null
     private val fromLocation: EditText by lazy { findViewById(R.id.editTextFrom) }
     private val toLocation: EditText by lazy { findViewById(R.id.editTextTo) }
     private lateinit var dbReference: DatabaseReference
@@ -56,26 +54,6 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
         setSupportActionBar(toolbar)
         this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 
-//        scheduleDbReference = FirebaseDatabase.getInstance().getReference("ScheduleRequests")
-//        val query = scheduleDbReference.orderByChild("rideId").equalTo("-Mw2u5Paxbg_eUzlMoPr")
-//        query.addListenerForSingleValueEvent(object : ValueEventListener {
-//            override fun onCancelled(dataSnapshot: DatabaseError) {
-//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//            }
-//            override fun onDataChange(dataSnapshot: DataSnapshot) {
-//                dataSnapshot.children.forEach { childSnapshot ->
-////                    Toast.makeText(
-////                        this@MainActivity,
-////                        childSnapshot.key,
-////                        Toast.LENGTH_SHORT
-////                    ).show()
-//
-//                    Log.i("name", childSnapshot.key.toString())
-//                    //val textValue = childSnapshot.child("text").getValue(String::class.java)
-//                }
-//            }
-//        })
-
         //retrieve userType (Driver/Rider) from homePageActivity
         if(intent.hasExtra("userIs")){
             userIs = intent.getStringExtra("userIs").toString()
@@ -83,6 +61,7 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
 
         //initializing scheduleRequestArrayList variable
         scheduleRequestArrayList = arrayListOf()
+        scheduleRequestArrayList.clear()
         scheduleRequestKeyArrayList = arrayListOf()
 
         val navView = findViewById<NavigationView>(R.id.navView)
@@ -101,9 +80,14 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
         validTripArrayList = arrayListOf()
         validTripIdArrayList = arrayListOf()
         dbReference = FirebaseDatabase.getInstance().getReference("Trips")
-
+        scheduleDbReference = FirebaseDatabase.getInstance().getReference("ScheduleRequests")
         // populating dashboard with all the trips available in database
-        getTripData()
+        getTripData(object : MyCallBack {
+            override fun onCallback(validTrip: ArrayList<String>){
+                Toast.makeText(this@MainActivity, validTrip[validTrip.size-1].toString(), Toast.LENGTH_SHORT).show()
+                getRequestData(validTrip)
+            }
+        })
 
         val submitButton = findViewById<Button>(R.id.btnFilter)
         submitButton.setOnClickListener {
@@ -117,10 +101,15 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
                     return@setOnClickListener
                 }
                 else -> {
-                    getTripData()
+                    getTripData(object : MyCallBack {
+                        override fun onCallback(validTrip: ArrayList<String>) {
+                            Toast.makeText(this@MainActivity, validTrip.size.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    })
                 }
             }
         }
+
         val clearFilterBtn = findViewById<Button>(R.id.btnClearFilter)
         clearFilterBtn.setOnClickListener {
             fromLocation.text = null
@@ -130,7 +119,11 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
 //            userType = null
             validTripArrayList.clear()
             validTripIdArrayList.clear()
-            getTripData()
+            getTripData(object : MyCallBack {
+                override fun onCallback(validTrip: ArrayList<String>) {
+                    Toast.makeText(this@MainActivity, validTrip.size.toString(), Toast.LENGTH_SHORT).show()
+                }
+            })
         }
 
         val addTripButton = findViewById<Button>(R.id.btnAddTrip)
@@ -140,60 +133,53 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
             startActivity(intent)
         }
 
-        scheduleDbReference = FirebaseDatabase.getInstance().getReference("ScheduleRequests")
-        getRequestData()
-//
-//        checkTripIDArrayListCount()
     }
 
+    override fun onStart() {
+        super.onStart()
+        scheduleRequestArrayList.clear()
+    }
     /*  get MyRidesData()
     *       1. Read data from schedule request table
     *       2. for all those data where status changed to accepted, see if the requesterId, or tripOwnerId is equal to uid
     *       3. If yes then send requesterId, uid, and tripId to the myRides page */
-
-    private fun getRequestData() {
-        scheduleDbReference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                scheduleRequestArrayList.clear()
-
-                // search for only those request where trip is valid
-                // query for all schedule requests based on ValidTripArrayList
-                if (snapshot.exists()) {
-                    for (scheduleSnapshot in snapshot.children) {
-                        val request = scheduleSnapshot.getValue(ScheduleRequestInfo::class.java)
+    private fun getRequestData(validTrip: ArrayList<String>) {
+        validTrip.forEach {
+            val query = scheduleDbReference.orderByChild("rideId").equalTo(it)
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(dataSnapshot: DatabaseError) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    scheduleRequestArrayList.clear()
+                    dataSnapshot.children.forEach { childSnapshot ->
+                        val request = childSnapshot.getValue(ScheduleRequestInfo::class.java)
                         if (request != null) {
-                            if(request.tripOwnerId == FirebaseAuth.getInstance().uid.toString() && request.status == "pending"){
+                            if (request.tripOwnerId == FirebaseAuth.getInstance().uid.toString() && request.status == "pending") {
                                 scheduleRequestArrayList.add(request)
-                                scheduleRequestKeyArrayList.add(scheduleSnapshot.key.toString())
+                                scheduleRequestKeyArrayList.add(childSnapshot.key.toString())
                             }
-                            if(request.requesterId == FirebaseAuth.getInstance().uid.toString() && request.status == "decline" && request.status != "Complete"){
+                            if (request.requesterId == FirebaseAuth.getInstance().uid.toString() && request.status == "decline" && request.status != "Complete") {
                                 scheduleRequestArrayList.add(request)
-                                scheduleRequestKeyArrayList.add(scheduleSnapshot.key.toString())
+                                scheduleRequestKeyArrayList.add(childSnapshot.key.toString())
                             }
 
-
+                        }
                             // if request.status is changed to decline, then store request data to another list to show user the notification
                             // that the ride request has been declined.
 
-                            /* if the trip has expired and the status is changed to remove then delete
-                               .. that instance from the scheduleRequest table */
-                        }
+                    }
+                    if(scheduleRequestArrayList.size != 0){
+                        Toast.makeText(this@MainActivity, "You have a notification.", Toast.LENGTH_LONG).show()
                     }
                 }
-                if(scheduleRequestArrayList.size != 0){
-                    Toast.makeText(this@MainActivity, "You have a notification.", Toast.LENGTH_LONG).show()
-                }
-            }
+            })
+        }
 
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
 
     //only fetch those rides whose date has not passed
-    private fun getTripData() {
+    private fun getTripData(myCallback : MyCallBack) {
         /* while fetching data for all the trips, check if the dates for particular rides are still valid
            if not valid then call removeNode() function to remove that node from the trips table
            Also store the Id of all the remove nodes inside a list
@@ -204,10 +190,11 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onDataChange(snapshot: DataSnapshot) {
                 validTripArrayList.clear()
+                validTripIdArrayList.clear()
                 if (snapshot.exists()){
                     for(tripSnapshot in snapshot.children){
                         val trip = tripSnapshot.getValue(TripsInfo::class.java)
-                        validTripIdArrayList.add(tripSnapshot.key.toString())
+//                        validTripIdArrayList.add(tripSnapshot.key.toString())
                         if (trip != null) {
                             if(checkTripExpiration(trip, tripSnapshot.key.toString()) == "Valid"){
                                 if(fromLocation.text.isEmpty()  && toLocation.text.isEmpty() ) { //&& userType.isNullOrEmpty()
@@ -215,7 +202,6 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
                                         Log.i("userType", "Match")
                                         validTripIdArrayList.add(tripSnapshot.key.toString())
                                         validTripArrayList.add(trip)
-                                        Toast.makeText(this@MainActivity, validTripArrayList.size.toString(), Toast.LENGTH_SHORT).show()
                                     }
                                 } else
                                     if(trip.lookingFor.equals(userIs)){
@@ -229,6 +215,7 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
                             }
                         }
                     }
+                    myCallback.onCallback(validTripIdArrayList)
                     tripRecyclerView.adapter = MyAdapter(validTripArrayList, this@MainActivity)
                 }
             }
@@ -240,9 +227,7 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
 
     }
 
-//    private fun checkTripIDArrayListCount() {
-//        Toast.makeText(this, this.validTripIdArrayList.size.toString(), Toast.LENGTH_SHORT).show()
-//    }
+
     /* Check the expiration of trips based on date and time.
        if (date is passed) {
             ride has expired
@@ -325,8 +310,66 @@ class MainActivity : AppCompatActivity(), MyAdapter.OnItemClickListener, OnNavig
 }
 
 
+/*scheduleDbReference.addValueEventListener(object : ValueEventListener {
+    override fun onDataChange(snapshot: DataSnapshot) {
+        scheduleRequestArrayList.clear()
+
+        // search for only those request where trip is valid
+        // query for all schedule requests based on ValidTripArrayList
+        if (snapshot.exists()) {
+            for (scheduleSnapshot in snapshot.children) {
+                val request = scheduleSnapshot.getValue(ScheduleRequestInfo::class.java)
+                if (request != null) {
+                    if(request.tripOwnerId == FirebaseAuth.getInstance().uid.toString() && request.status == "pending"){
+                        scheduleRequestArrayList.add(request)
+                        scheduleRequestKeyArrayList.add(scheduleSnapshot.key.toString())
+                    }
+                    if(request.requesterId == FirebaseAuth.getInstance().uid.toString() && request.status == "decline" && request.status != "Complete"){
+                        scheduleRequestArrayList.add(request)
+                        scheduleRequestKeyArrayList.add(scheduleSnapshot.key.toString())
+                    }
 
 
+                    // if request.status is changed to decline, then store request data to another list to show user the notification
+                    // that the ride request has been declined.
+
+                    *//* if the trip has expired and the status is changed to remove then delete
+                               .. that instance from the scheduleRequest table *//*
+                        }
+                    }
+                }
+                if(scheduleRequestArrayList.size != 0){
+                    Toast.makeText(this@MainActivity, "You have a notification.", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })*/
+/*
+        scheduleDbReference = FirebaseDatabase.getInstance().getReference("ScheduleRequests")
+        val query = scheduleDbReference.orderByChild("rideId").equalTo("-Mw2u5Paxbg_eUzlMoPr")
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(dataSnapshot: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                dataSnapshot.children.forEach { childSnapshot ->
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        childSnapshot.key,
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+
+                    Log.i("name", childSnapshot.key.toString())
+                    //val textValue = childSnapshot.child("text").getValue(String::class.java)
+                }
+            }
+        })
+
+ */
 //    fun onRadioButtonClicked(view: View){
 //        if (view is RadioButton) {
 //            // Is the button now checked?
---
+++ app/src/main/java/com/example/hitchhike/ui/interfaces/MyCallBack.kt
@@ -0,0 +1,7 @@
+package com.example.hitchhike.ui.interfaces
+
+import com.example.hitchhike.model.TripsInfo
+
+interface MyCallBack {
+    fun onCallback(value: ArrayList<String>)
+}
