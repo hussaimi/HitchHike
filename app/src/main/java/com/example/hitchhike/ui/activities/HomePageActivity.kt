6285a118c676a846b4b364f584e8ab6c5c633a59
Muhammad Moiz Hussain <mohammadmoiz6@gmail.com>
2022-03-09 18:36:26 -0500

    Added Logout option in HomePage

--- app/src/main/java/com/example/hitchhike/ui/activities/HomePageActivity.kt
+++ app/src/main/java/com/example/hitchhike/ui/activities/HomePageActivity.kt
@@ -1,10 +1,14 @@
 package com.example.hitchhike.ui.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hitchhike.R
 import com.example.hitchhike.databinding.ActivityHomePageBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class HomePageActivity : AppCompatActivity() {
 
@@ -27,4 +31,28 @@ class HomePageActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.appbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) : Boolean {
+        when (item.itemId) {
+            R.id.logoutMenu -> {
+                logout()
+                true
+            }
+        }
+        return true
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
